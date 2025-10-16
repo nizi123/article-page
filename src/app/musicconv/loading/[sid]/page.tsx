8798676a5ed10b0f0104c3a1fd5f1a66465b5cd8
nizi123 <hee981226@naver.com>
2025-10-17 03:00:23 +0900

    타입 오류 수정

--- src/app/musicconv/loading/[sid]/page.tsx
+++ src/app/musicconv/loading/[sid]/page.tsx
@@ -1,20 +1,20 @@
 import MusicConvClient from "../../MusicConvClient";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default function Page({
-  params,
-  searchParams,
-}: {
-  params: { sid: string };
-  searchParams: { q?: string; n?: string };
-}) {
+// params/searchParams가 빌드 환경에 따라 Promise로 추론되는 케이스 회피
+export default function Page(props: any) {
+  const sid = (props?.params?.sid as string) ?? "";
+  const q = (props?.searchParams?.q as string) ?? "";
+  const n = (props?.searchParams?.n as string) ?? "";
+
   return (
     <MusicConvClient
       initialView="loading"
-      sid={params.sid}
-      initialText={searchParams?.q ?? ""}
-      initialNickname={searchParams?.n ?? ""}
+      sid={sid || null}
+      initialText={q}
+      initialNickname={n}
     />
   );
 }
--- src/app/musicconv/result/[sid]/page.tsx
+++ src/app/musicconv/result/[sid]/page.tsx
@@ -1,20 +1,20 @@
 import MusicConvClient from "../../MusicConvClient";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default function Page({
-  params,
-  searchParams,
-}: {
-  params: { sid: string };
-  searchParams: { q?: string; n?: string };
-}) {
+// params/searchParams 타입을 any로 받아 Promise/객체 모두 커버
+export default function Page(props: any) {
+  const sid = (props?.params?.sid as string) ?? "";
+  const q = (props?.searchParams?.q as string) ?? "";
+  const n = (props?.searchParams?.n as string) ?? "";
+
   return (
     <MusicConvClient
       initialView="result"
-      sid={params.sid}
-      initialText={searchParams?.q ?? ""}
-      initialNickname={searchParams?.n ?? ""}
+      sid={sid || null}
+      initialText={q}
+      initialNickname={n}
     />
   );
 }
