aa96ba4075b571c53438376437cd1914a08eaf72
nizi123 <hee981226@naver.com>
2026-01-04 20:46:01 +0900

    리다이렉팅

--- next.config.ts
+++ next.config.ts
@@ -12,4 +12,24 @@ const nextConfig: NextConfig = {
   },
 };
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'labchasm.com' }],
+        destination: 'https://labchasm.framer.website/',
+        permanent: true, // 308
+      },
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'www.labchasm.com' }],
+        destination: 'https://labchasm.framer.website/',
+        permanent: true, // 308
+      },
+    ];
+  },
+};
+
 export default nextConfig;
